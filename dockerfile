6a5fd83db4693f36a7d30a5096ab9a7d87cfb864
EnriquePlayGroupLatam <enrique@playgrouplatam.com>
2025-11-27 14:24:25 -0500

    135.

--- dockerfile
+++ dockerfile
@@ -6,8 +6,18 @@ COPY package.json ./
 COPY package-lock.json ./
 
 
+
 RUN npm install
 
+# 2. Ahora sí copiamos TODO el proyecto (incluye prisma/)
 COPY . .
 
+# 3. Generar Prisma Client correctamente
+RUN npx prisma generate
+
+# 4. Compilar NestJS
+RUN npm run build
+
 EXPOSE 3002
+
+CMD ["npm", "run", "start:prod"]
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class AuthService {}
+export class AuthService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy {
+
+    private readonly logger = new Logger('AuthService');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('MongoDB Connected');
+  }
+
+  onModuleDestroy() {
+    this.$disconnect();
+    
+  }
+}
